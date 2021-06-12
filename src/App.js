61c99f49619800c93ae79db18342ceb8204771d6
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-06-12 10:07:39 +0200

    add app content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1 className="container__header">Walutomat - Twój kantor w sieci </h1>
+        <form className="form js-form">
+            <label className="form__label form__label--higlighted">
+                Wpisz kwotę*:
+                <input className="form__input js-input" type="number" min="0.1" step="any" required
+                    placeholder="wpisz kwotę w PLN"/>
+            </label>
+            <h2>Wybierz Walute:</h2>
+            <ul className="form__listRadio">
+                <li>
+                    <label className="form__label">
+                        <input className="form__input js-currency" type="radio" name="currency" value="eur"/>
+                        Euro
+                    </label>
+                </li>
+                <li>
+                    <label className="form__label">
+                        <input className="form__input js-currency" type="radio" name="currency" value="usd"/>
+                        Dolar Amerykański
+                    </label>
+                </li>
+                <li> <label className="form__label">
+                        <input className="form__input js-currency" type="radio" name="currency" value="pound"/>
+                        Funt Brytyjski
+                    </label>
+                </li>
+            </ul>
+            <button className="button js-button">
+                Oblicz kurs
+            </button>
+        </form>
+        <p className="result js-result"></p>
+        <footer>
+            2021&copy; Wszelkie prawa zastrzeżone!!!
+        </footer>
     </div>
   );
 }
