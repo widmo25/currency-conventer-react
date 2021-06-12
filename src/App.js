3c834f9860d9082a27899f7e07029137c5c139b2
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-06-12 10:38:40 +0200

    add Container Component and add styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Form from "./Form";
-
+import Container from "./Container"
 
 function App() {
-  return (
-    <div className="container">
-        <h1 className="container__header">Walutomat - Twój kantor w sieci </h1>
-        <Form />
-        <p className="result js-result"></p>
-        <footer>
-            2021&copy; Wszelkie prawa zastrzeżone!!!
-        </footer>
-    </div>
-  );
+    return (
+        <Container>
+            <h1 className="container__header">Walutomat - Twój kantor w sieci </h1>
+            <Form />
+            <p className="result js-result"></p>
+            <footer>
+                2021&copy; Wszelkie prawa zastrzeżone!!!
+            </footer>
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/Container/index.js
@@ -0,0 +1,10 @@
+import "./style.css";
+const Container = (props) =>{
+    return (
+        <div className="container">
+            {props.children}
+        </div>
+    )
+}
+
+export default Container
