5eed0856bf1005f430b058fe53a6785fb3fe8905
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-06-15 15:36:17 +0200

    Formatting code

--- src/Form/index.js
+++ src/Form/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 
 const Form = () => {
 
-    const onFormSubmit = (event) =>{
+    const onFormSubmit = (event) => {
         event.preventDefault();
     }
 
@@ -12,31 +12,48 @@ const Form = () => {
         <form className="form" onSubmit={onFormSubmit}>
             <label className="form__label">
                 Wpisz kwotę*:
-                <input className="form__input" type="number" min="0.1" step="any" required
+                <input
+                    className="form__input"
+                    type="number"
+                    min="0.1"
+                    step="any"
+                    required
                     placeholder="wpisz kwotę w PLN" />
             </label>
             <h2>Wybierz Walute:</h2>
             <ul className="form__listRadio">
                 <li>
                     <label className="form__label">
-                        <input className="form__input" type="radio" name="currency" value="eur" />
+                        <input
+                            className="form__input"
+                            type="radio"
+                            name="currency"
+                            value="eur" />
                         Euro
                     </label>
                 </li>
                 <li>
                     <label className="form__label">
-                        <input className="form__input" type="radio" name="currency" value="usd" />
+                        <input
+                            className="form__input"
+                            type="radio"
+                            name="currency"
+                            value="usd" />
                         Dolar Amerykański
                     </label>
                 </li>
                 <li>
                     <label className="form__label">
-                        <input className="form__input" type="radio" name="currency" value="pound" />
+                        <input
+                            className="form__input"
+                            type="radio"
+                            name="currency"
+                            value="pound" />
                         Funt Brytyjski
                     </label>
                 </li>
             </ul>
-            <Button/>
+            <Button />
         </form>
     )
 }
