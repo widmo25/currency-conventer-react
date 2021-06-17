82867ff1d142b254564a2e5f9ca76dbd39bf560d
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-06-17 11:20:12 +0200

    add currencies.js

---
+++ src/currencies.js
@@ -0,0 +1,19 @@
+export const currencies = () => [
+    {
+        short: "usd",
+        name: "Dolar Amerykański",
+        rate: 3.78,
+    },
+    {
+        short: "eur",
+        name: "Euro",
+        rate: 4.55,
+    },
+    {
+        short: "gbp",
+        name: "Funt Brytyjski",
+        rate: 5.27
+    }
+]
+
+
