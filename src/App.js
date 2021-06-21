fcfd55ef1012d8c031bdf3581963a57a5b11a7c2
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-06-21 12:01:59 +0200

    add calculateResult

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import Form from "./Form";
+import {Form} from "./Form";
 import Container from "./Container"
 import Footer from "./Footer"
 import Result from "./Result"
+import { useState } from "react";
+import { currencies } from "./currencies";
+
 
 function App() {
+    const [result, setResult] = useState("")
+
+    const calculateResult = (amount, currency) => {
+        const rate = currencies.find(({short}) => short === currency).rate;
+
+        setResult({
+            sourceAmount: +amount,
+            targetAmount: amount / rate,
+            currency,
+        });
+    }
+
     return (
         <Container>
             <h1 className="container__header">Walutomat - Twój kantor w sieci </h1>
-            <Form />
-            <Result />
+            <Form calculateResult={calculateResult} />
+            <Result result={result}/>
             <Footer />
         </Container>
     );
