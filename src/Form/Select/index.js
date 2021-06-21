54f00a53a705b0c2bef9da7f50a96b942a942d64
Michał Wodniczak <michal.wodniczak@o2.pl>
2021-06-21 17:37:18 +0200

    rename Compent name

---
+++ src/Form/Select/index.js
@@ -0,0 +1,31 @@
+import { currencies } from "../../currencies"
+
+const Select = ({ currency, setCurrency }) => {
+
+
+
+    return (
+        <ul className="form__listSelect">
+            <label className="form__label">
+                <select
+                    className="form__input"
+                    value={currency}
+                    onChange={({ target }) => setCurrency(target.value)}
+                >
+
+                    {currencies.map((currency => (
+                        <option
+                            key={currency.short}
+                            value={currency.short}
+                        >
+                            {currency.name}
+                        </option>
+                    )))}
+                </select>
+            </label>
+        </ul >
+    )
+
+}
+
+export { Select }
